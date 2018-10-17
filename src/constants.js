717d1c77af848624b92d13e0e9372a4a9f1d455c
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-10-16 22:41:11 -0700

    refactor checkpoint

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,13 @@
 import PropTypes from 'prop-types';
-import axios from "axios";
 
 export const EMPTY_FUNC = () => {};
 export const EMPTY_OBJ = {};
 export const DATE_FORMAT = "MMM D YYYY";
-export const NUM_ARTICLES = 30;
-export const NUM_YEARS_BACK = 12;
+export const NUM_ARTICLES = 500;
+export const NUM_YEARS_BACK = 100;
 export const ACCEPTED_FILE_TYPES = [
   ".doc",
   ".docx",
-  ".xml",
   "application/msword",
   "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
   "text/plain",
@@ -31,60 +29,62 @@ export const STATUSES = [
   "waitlisted"
 ];
 
-// TODO LEGACY REFACTOR
-// export function SEARCH_COLLEGE_OPTIONS(name) {
-//   if (!name) {
-//     return Promise.resolve({
-//       options: []
-//     });
-//   }
-//   return axios
-//     .get(`https://universities.hipolabs.com/search?name=${name}`)
-//     .then(res => JSON.parse(res))
-//     .then(data => data.data.map(c => c.name))
-//     .then(colleges => convertToOptions(colleges))
-//     .then(options => ({
-//       options
-//     }))
-//     .catch(err => console.error(err));
-// }
+const MAX_TRIES = 5;
 
-// export const IMAGE_IDS = axios
-//   .get("https://picsum.photos/list")
-//   .then(res => res.data)
-//   .then(images => images.map(i => i.id))
-//   .catch(function () {
-//     console.error("error");
-//   });
+function getImage(tries = 0) {
+  return new Promise((resolve, reject) => {
+    axios.get('https://source.unsplash.com/random').then(res => resolve(res.data)).catch(err => {
+      if (tries == MAX_TRIES) {
+        return reject(err);
+      }
+      console.error(` error happened: ${err}. \n now retyring`);
+      return getImage(tries + 1);
+    })
+  });
+}
 
-// TODO REFACTOR
+export async function getImages() {
+  return await Promise.all(
+    Array(NUM_ARTICLES).keys().map(idx => getImage())
+  );
+}
+
 export const ESSAYS_SHAPE = PropTypes.shape({
-  id: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
   paragraphs: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-  meta: PropTypes.shape({
-    tag: PropTypes.string.isRequired,
-    prompt: PropTypes.string.isRequired,
-    college: PropTypes.string.isRequired,
-    yearApplied: PropTypes.number.isRequired,
-    applicationStatus: PropTypes.string.isRequired,
-    dateUploaded: PropTypes.object.isRequired,
-    views: PropTypes.number.isRequired,
-    commentsCount: PropTypes.number.isRequired,
-    imageNo: PropTypes.object.isRequired
-  }),
-  links: PropTypes.shape({
-    linkFacebookShare: PropTypes.string.isRequired,
-    linkTwitterShare: PropTypes.string.isRequired,
-    linkBasedOnAuthor: PropTypes.string.isRequired,
-    linkBasedOnYearApplied: PropTypes.string.isRequired,
-    linkBasedOnCountry: PropTypes.string.isRequired,
-    linkBasedOnApplicationStatus: PropTypes.string.isRequired,
-    linkBasedOnCollege: PropTypes.string.isRequired,
-    linkBasedOnPrompt: PropTypes.string.isRequired
-  }),
-  author: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    country: PropTypes.string.isRequired
-  }).isRequired
+  prompt: PropTypes.string.isRequired,
+  college: PropTypes.string.isRequired,
+  yearApplied: PropTypes.number.isRequired,
+  author: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  applicationStatus: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  dateUploaded: PropTypes.instanceOf(Date),
+  imageLink: PropTypes.string.isRequired,
 }).isRequired;
+
+// TODO LEGACY
+function getSearchResults(collegeName) {
+  if (!collegeName) {
+    return Promise.resolve({
+      options: []
+    });
+  }
+  return axios
+    .get(`https://universities.hipolabs.com/search?name=${collegeName}`)
+    .then(res => JSON.parse(res))
+    .then(data => data.data.map(c => c.name))
+    .then(options => ({
+      options
+    }))
+    .catch(err => console.error(err));
+}
+
+function _getImages() {
+  axios
+    .get("https://picsum.photos/list")
+    .then(res => res.data)
+    .catch(function () {
+      console.error("error");
+    });
+}
--- src/data/essays.js
+++ src/data/essays.js
@@ -6,25 +6,23 @@ import {
   NUM_YEARS_BACK,
   COLLEGES
 } from "../constants";
-
 import {
-  selectRandom
+  selectRandom,
+  getRandomInt
 } from "../utils";
 
+const RECENT_DAYS = 30;
 export default Array.from(Array(NUM_ARTICLES), () =>
   ({
     id: faker.random.uuid(),
     paragraphs: faker.lorem.paragraphs(),
     prompt: faker.lorem.paragraph(),
     college: selectRandom(COLLEGES),
-    yearApplied: faker.date.past(Array.from(Array(NUM_YEARS_BACK).keys())),
+    yearApplied: faker.date.past(getRandomInt(0, NUM_YEARS_BACK)),
     author: faker.name.findName(),
     email: faker.internet.email(),
     applicationStatus: selectRandom(STATUSES),
     tag: selectRandom(TAGS),
-    dateUploaded: faker.date.recent(),
-    imageNo: IMAGE_IDS.then(ids => {
-      return selectRandom(ids);
-    })
-
-  }))
+    dateUploaded: faker.date.recent(RECENT_DAYS),
+    imageLink: faker.image.people()
+  }));
