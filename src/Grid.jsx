5bcbe561361308861649013e183a8da62126e6df
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-10-26 12:53:00 -0700

    improved to top

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,40 +1,18 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent, Fragment, createRef } from 'react';
 import PropTypes from 'prop-types';
 import { ESSAYS_SHAPE } from './constants';
 import Card from './Card';
 
-class ToTop extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: 'none'
-    };
-  }
-
-  componentDidMount() {
-    window.onscroll = () => {
-      this.setState({
-        display: 'block'
-      });
-    };
-  }
-
-  render() {
-    const { display } = this.state;
-    return (
-      <div
-        className="uk-section uk-section-secondary uk-section-xsmall uk-padding-remove-horizontal"
-        style={{ display }}
-      >
-        <div className="uk-container uk-container-expand">
-          <div className="uk-flex uk-flex-right">
-            <button uk-totop="" uk-scroll="" />
-          </div>
+const ToTop = ({ numChildren }) =>
+  numChildren > 4 && (
+    <div className="uk-section uk-section-secondary uk-section-xsmall uk-padding-remove-horizontal">
+      <div className="uk-container uk-container-expand">
+        <div className="uk-flex uk-flex-right">
+          <button uk-totop="" uk-scroll="" />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
 
 const FilterAndSort = () => (
   <Fragment>
@@ -72,55 +50,81 @@ const FilterAndSort = () => (
   </Fragment>
 );
 
-const Grid = ({ essays }) => (
-  <div>
-    {essays.length > 0 ? (
-      <div uk-filter="target: .js-filter">
-        <FilterAndSort />
-        <div
-          className="js-filter uk-grid uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m uk-child-width-1-4@xl uk-section-muted"
-          uk-grid=""
-          uk-scrollspy="cls: uk-animation-fade; target: > div > .uk-card; delay: 500;"
-        >
-          {essays.map(
-            ({
-              id,
-              paragraphs,
-              tag,
-              dateUploaded,
-              imageLink,
-              college,
-              applicationStatus
-            }) => (
-              <div key={id} data-tag={tag} data-date={dateUploaded.valueOf()}>
-                <Card
-                  text={paragraphs[0]}
-                  tag={tag}
-                  imageLink={imageLink}
-                  linkEssay={`/essays/${id}`}
-                  college={college}
-                  applicationStatus={applicationStatus}
-                />
-              </div>
-            )
-          )}
-        </div>
-        <div>
-          <div className="uk-section uk-section-muted uk-section-xsmall" />
-          <div className="uk-container" />
-          <ToTop />
-        </div>
+class Grid extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.grid = createRef();
+    this.state = {
+      numChildren: 0
+    };
+  }
+
+  componentDidMount() {
+    const numChildren = this.grid.current.childElementCount;
+    this.setState({
+      numChildren
+    });
+  }
+
+  render() {
+    const { essays } = this.props;
+
+    return (
+      <div>
+        {essays.length > 0 ? (
+          <div uk-filter="target: .js-filter">
+            <FilterAndSort />
+            <div
+              className="js-filter uk-grid uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m uk-child-width-1-4@xl uk-section-muted"
+              uk-grid=""
+              uk-scrollspy="cls: uk-animation-fade; target: > div > .uk-card; delay: 500;"
+              ref={this.grid}
+            >
+              {essays.map(
+                ({
+                  id,
+                  paragraphs,
+                  tag,
+                  dateUploaded,
+                  imageLink,
+                  college,
+                  applicationStatus
+                }) => (
+                  <div
+                    key={id}
+                    data-tag={tag}
+                    data-date={dateUploaded.valueOf()}
+                  >
+                    <Card
+                      text={paragraphs[0]}
+                      tag={tag}
+                      imageLink={imageLink}
+                      linkEssay={`/essays/${id}`}
+                      college={college}
+                      applicationStatus={applicationStatus}
+                    />
+                  </div>
+                )
+              )}
+            </div>
+            <div>
+              <div className="uk-section uk-section-muted uk-section-xsmall" />
+              <div className="uk-container" />
+              <ToTop numChildren={this.state.numChildren} />
+            </div>
+          </div>
+        ) : (
+          <div
+            className="uk-flex uk-flex-center uk-flex-middle"
+            uk-height-viewport="expand: true"
+          >
+            <p>nothing to show</p>
+          </div>
+        )}
       </div>
-    ) : (
-      <div
-        className="uk-flex uk-flex-center uk-flex-middle"
-        uk-height-viewport="expand: true"
-      >
-        <p>nothing to show</p>
-      </div>
-    )}
-  </div>
-);
+    );
+  }
+}
 
 Grid.propTypes = {
   essays: PropTypes.arrayOf(ESSAYS_SHAPE).isRequired
