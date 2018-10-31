6502fd0793bf04927bfb960de6aba252351cfb31
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-10-31 11:00:20 -0700

    resized images

--- src/Form.jsx
+++ src/Form.jsx
@@ -13,29 +13,27 @@ const Form = () => (
         <h1 className="uk-text-center">{UPLOAD_TITLE}</h1>
       </div>
       <div
-        className="uk-grid-collapse uk-flex-middle uk-child-width-1-2@m"
+        className="uk-modal-body uk-grid-collapse uk-child-width-1-2@m"
         uk-grid=""
       >
         <div>
-          <div className="uk-modal-body">
-            <iframe
-              title="submit your essay"
-              uk-responsive=""
-              width="640"
-              height="800"
-              src="https://docs.google.com/forms/d/e/1FAIpQLSd2KZfeAHiZ_UcxLtzs-3gwzP0mXPN_4HCTQKZ6aiB5764BOg/viewform?embedded=true"
-            >
-              Loading...
-            </iframe>
-          </div>
+          <iframe
+            title="submit your essay"
+            uk-responsive=""
+            width="640"
+            height="850"
+            src="https://docs.google.com/forms/d/e/1FAIpQLSd2KZfeAHiZ_UcxLtzs-3gwzP0mXPN_4HCTQKZ6aiB5764BOg/viewform?embedded=true"
+          >
+            Loading...
+          </iframe>
         </div>
 
         <div className="uk-inline uk-flex-first">
           <div
             className="uk-background-cover uk-visible@m"
-            uk-height-viewport=""
             data-src={UPLOAD_IMAGE}
             uk-img=""
+            uk-height-viewport=""
           />
           <div className="uk-overlay uk-overlay-default uk-position-cover" />
         </div>
